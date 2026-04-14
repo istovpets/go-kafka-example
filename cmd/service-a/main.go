b9a04d91979b3b6e5e89409c0d61d9a9534d8818
Igor Stovpets <igor@stovpets.com>
2026-04-14 03:17:49 +0300

    add: consumer & producer to main.go

--- cmd/service-a/main.go
+++ cmd/service-a/main.go
@@ -1,5 +1,66 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
 func main() {
 	println("service-a started")
+
+	seeds := []string{"localhost:9092"}
+	cl, err := kgo.NewClient(
+		kgo.SeedBrokers(seeds...),
+		kgo.DialTimeout(5*time.Second),
+		kgo.ConsumerGroup("my-group-identifier"),
+		kgo.ConsumeTopics("foo"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer cl.Close()
+
+	ctx := context.Background()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	record := &kgo.Record{Topic: "foo", Value: []byte("bar")}
+	cl.Produce(ctx, record, func(_ *kgo.Record, err error) {
+		defer wg.Done()
+		if err != nil {
+			fmt.Printf("record had a produce error: %v\n", err)
+		}
+	})
+	wg.Wait()
+
+	if err := cl.ProduceSync(ctx, record).FirstErr(); err != nil {
+		fmt.Printf("record had a produce error while synchronously producing: %v\n", err)
+	}
+
+	for {
+		fetches := cl.PollFetches(ctx)
+		if errs := fetches.Errors(); len(errs) > 0 {
+			panic(fmt.Sprint(errs))
+		}
+
+		iter := fetches.RecordIter()
+		for !iter.Done() {
+			record := iter.Next()
+			fmt.Println(string(record.Value), "from an iterator!")
+		}
+
+		fetches.EachPartition(func(p kgo.FetchTopicPartition) {
+			for _, record := range p.Records {
+				fmt.Println(string(record.Value), "from range inside a callback!")
+			}
+
+			p.EachRecord(func(record *kgo.Record) {
+				fmt.Println(string(record.Value), "from a second callback!")
+			})
+		})
+	}
 }
